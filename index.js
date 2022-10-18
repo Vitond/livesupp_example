029dc97d495b6fa9ce581d49757edbc8aa2a8183
Ondřej Vitula <vitula@tectronik.cz>
2022-10-18 12:50:40 +0200

    Change signPost to Cyrrus

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-console.log('V2');
+console.log('V3');
 
 window.addEventListener('LIVESUPP_LOADED', async event => {
     const lsAPI = event.detail.api
-    lsAPI.useSignpost()
+    let fields = [
+        {
+            type: 'name',
+            label: 'Your name',
+        },
+        {
+            type: 'either',
+            fields: [
+                {
+                    type: 'phoneNumber',
+                    label: 'Phone number',
+                },
+                {
+                    type: 'email',
+                    label: 'Email',
+                },
+            ],
+        }                
+    ];        let choices = [
+        {
+            label: 'Praha',
+            pageId: 697058,
+        },
+        {
+            label: 'Brno',
+            pageId: 697059,
+        },
+        {
+            label: 'Ostrava',
+            pageId: 697060,
+        }
+    ];        let steps = [
+        {
+            fields: fields,
+            nextStepButtonText: "Continue",
+            title: "Let's get to know you a bit...",
+            type: "contact"
+        },
+        {
+            choices: choices,
+            nextStepButtonText: "Finish",
+            title: "Choose an office",
+            type: "page-redirection"
+        },
+        {
+            type: "assign-me"
+        }
+    ];        
+    let signPost = {
+        steps: steps,
+        virtualOperator: {
+            avatarUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX////0eSnzcAv+9e/+8OfzbgD84NP0dBf++PX4tZX",
+            name: "John Doe",
+            title: "Customer Support"
+        }
+    }
+    lsAPI.useSignpost(signPost);
  })
